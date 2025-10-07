e2774a89f1f11acc6781fb713eb2f3efa868eb4c
Leonardo <leonardo.braga4@fatec.sp.gov.br>
2025-10-06 21:56:08 -0300

    feat: eureka

---
+++ ms-eurekaserver/src/test/java/io/github/Leonardo/ms_eurekaserver/MsEurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Leonardo.ms_eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
